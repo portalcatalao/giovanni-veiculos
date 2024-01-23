bfbbbde3bdd861bc2384b44d86e080d2f7686dd9
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-23 09:18:16 -0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,26 @@ import { CardVehicle } from "@/components/card-vehicle";
 import { FilterHome } from "@/components/forms/filter-home";
 import { fetchData } from "@/hooks/useFetch";
 import { IBrand, IVehicle } from "@/interfaces/vehicle";
+import Image from "next/image";
 
 export default async function Home() {
-  const {data: brands}: {data: Array<IBrand>} = await fetchData('fipe/car/brand/list');
+  const { data: brands }: { data: Array<IBrand> } = await fetchData('fipe/car/brand/list');
   const { data }: { data: { result: { vehicles: Array<IVehicle> } } } = await fetchData('ad/list?limit=8');
   const vehicles = data.result.vehicles;
 
   return (
     <main>
       <div className="bg-zinc-950">
-        <div className="w-full max-w-7xl max-2xl:px-4 py-8 flex flex-col items-center mx-auto">
-          <h1 className="text-2xl lg:text-4xl font-semibold max-w-lg text-center">Descubra o Veículo Perfeito na Giovanni Veículos</h1>
-          <FilterHome brands={brands}/>
+        <div className="relative max-lg:h-fit h-[60vh]">
+          <div className="max-lg:hidden absolute top-0 left-0 right-0 bottom-0">
+            <Image src={'/banner-home.jpg'} alt="Banner Home Giovanni Veículos" fill sizes="100%" className="object-cover" />
+          </div>
+          <div className="max-lg:relative absolute top-0 left-0 right-0 bottom-0 bg-black/70">
+            <div className="w-full h-full max-w-7xl max-2xl:px-4 py-8 flex flex-col justify-center items-center mx-auto">
+              <h1 className="text-2xl lg:text-4xl font-semibold max-w-lg text-center">Descubra o Veículo Perfeito na Giovanni Veículos</h1>
+              <FilterHome brands={brands} />
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full max-w-7xl max-2xl:px-4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 my-8">
