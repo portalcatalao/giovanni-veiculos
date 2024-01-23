530cdb6d7f77c3980bddb201a53b78b429de2a92
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-23 10:32:43 -0300

    Commit

--- src/app/veiculos/[...id]/page.tsx
+++ src/app/veiculos/[...id]/page.tsx
@@ -1,11 +1,40 @@
 import { fetchData } from "@/hooks/useFetch";
 import { IVehicle } from "@/interfaces/vehicle";
 import VehicleViewContent from "./content";
+import { Metadata, ResolvingMetadata } from "next";
+
+interface Props {
+    params: {
+        id: Array<string>;
+    },
+    searchParams?: {
+        page: number;
+    }
+}
+
+export async function generateMetadata(
+    { params }: Props,
+    parent?: ResolvingMetadata,
+): Promise<Metadata> {
+    const versionId = params.id[params.id.length - 1].split('_');
+    const id = versionId[versionId.length - 1];
+    const {data: vehicle}: {data: IVehicle} = await fetchData(`ad/${id}/view`);
+
+    return {
+        title: `${vehicle.version.model.brand.name} ${vehicle.version.model.name} - ${vehicle.version.name} - Giovanni Veículos Multimarcas`.toUpperCase(),
+        description: `${vehicle.version.model.brand.name} ${vehicle.version.model.name} - ${vehicle.version.name}. Faça nos uma visita para conhecer este e outros de nossos veículos`,
+        openGraph: {
+            title: `${vehicle.version.model.brand.name} ${vehicle.version.model.name} - ${vehicle.version.name} - Giovanni Veículos Multimarcas`.toUpperCase(),
+            description: `${vehicle.version.model.brand.name} ${vehicle.version.model.name} - ${vehicle.version.name}. Faça nos uma visita para conhecer este e outros de nossos veículos`,
+            images: ['https://portalautos.com.br/' + vehicle.gallery?.images[0]?.path]
+        }
+    }
+}
 
 export default async function VehicleViewPage({ params }: { params: { id: Array<String> } }) {
     const versionId = params.id[params.id.length - 1].split('_');
     const id = versionId[versionId.length - 1];
-    const {data: vehicle}: {data: IVehicle} = await fetchData(`ad/${id}/view`, 0);
+    const {data: vehicle}: {data: IVehicle} = await fetchData(`ad/${id}/view`);
 
     return (
         <VehicleViewContent vehicle={vehicle} />
