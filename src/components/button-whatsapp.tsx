4cd496562f6e9c36df2250aa798098f443cd707d
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-26 15:31:08 -0300

    Commit

--- src/components/button-whatsapp.tsx
+++ src/components/button-whatsapp.tsx
@@ -2,8 +2,17 @@ import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
 
 export function ButtonWhatsapp() {
+    const numeros = [
+        "5564996996215",
+        "5564992496844",
+        "5564999415975",
+        "5564996955520"
+    ];
+    
+    const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+
     return (
-        <Link href="https://api.whatsapp.com/send?phone=556492496844&text=Giovanni Veículos Multimarcas agradece o seu contato" target="_blank" className="fixed z-[9999] bottom-4 right-4 px-2 py-2 bg-green-600 hover:bg-green-700 transition-colors rounded-md text-white">
+        <Link href={`https://api.whatsapp.com/send?phone=${numeroAleatorio}&text=Giovanni Veículos Multimarcas agradece o seu contato" target="_blank" className="fixed z-[9999] bottom-4 right-4 px-2 py-2 bg-green-600 hover:bg-green-700 transition-colors rounded-md text-white`}>
             <FaWhatsapp size={24}/>
         </Link>
     )
--- src/components/forms/proposal-form.tsx
+++ src/components/forms/proposal-form.tsx
@@ -14,10 +14,19 @@ export function ProposalForm({ vehicle }: { vehicle: IVehicle }) {
     const description = useForm();
 
     const handleSendProposal = () => {
+        const numeros = [
+            "5564996996215",
+            "5564992496844",
+            "5564999415975",
+            "5564996955520"
+        ];
+        
+        const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+
+
         if (name.validate() && email.validate() && phone.validate() && description.validate()) {
-            const wpp = "556492496844";
             const text = encodeURIComponent(`\nNome: ${name.value}\nEmail: ${email.value}\nTelefone: ${phone.value}\n Mensagem: ${description.value}`);
-            const whatsappLink = `https://api.whatsapp.com/send?phone=${wpp}&text=${text}`;
+            const whatsappLink = `https://api.whatsapp.com/send?phone=${numeroAleatorio}&text=${text}`;
 
             window.open(whatsappLink, "_blank");
         }
