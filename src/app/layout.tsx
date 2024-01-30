ea627698c66066ea551f0b559c5f717ba39b8a00
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-30 09:31:26 -0300

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={inter.className + ' bg-zinc-900'}>
         <div className="flex flex-col min-h-screen w-full">
           <Header />
