57007b89cdebc9445c51f82a1e35bf97b062a4ec
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-01-30 09:53:52 -0300

    Commit

--- src/components/button-whatsapp.tsx
+++ src/components/button-whatsapp.tsx
@@ -12,7 +12,7 @@ export function ButtonWhatsapp() {
     const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
 
     return (
-        <Link href={`https://api.whatsapp.com/send?phone=${numeroAleatorio}&text=Giovanni Veículos Multimarcas agradece o seu contato" target="_blank" className="fixed z-[9999] bottom-4 right-4 px-2 py-2 bg-green-600 hover:bg-green-700 transition-colors rounded-md text-white`}>
+        <Link href={`https://api.whatsapp.com/send?phone=${numeroAleatorio}&text=Giovanni Veículos Multimarcas agradece o seu contato`} target="_blank" className={`fixed z-[9999] bottom-4 right-4 px-2 py-2 bg-green-600 hover:bg-green-700 transition-colors rounded-md text-white`}>
             <FaWhatsapp size={24}/>
         </Link>
     )
