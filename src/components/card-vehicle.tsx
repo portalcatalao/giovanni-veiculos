29c2e6002772d1c4f42523f77c0a66b6eb882463
Portal Catalão Internet Service Ltda. <github@portalcatalao.com.br>
2024-07-23 17:28:17 -0300

    Update card-vehicle.tsx

--- src/components/card-vehicle.tsx
+++ src/components/card-vehicle.tsx
@@ -5,7 +5,7 @@ export function CardVehicle({ vehicle }) {
     return (
         <Link href={`/veiculos/${vehicle.version.model.brand.id_string}/${vehicle.version.model.id_string}/${vehicle.version.id_string}_${vehicle.id}`} className="p-3 bg-zinc-800 rounded-md">
             <div className="w-full h-[248px] relative rounded overflow-hidden">
-                <img src={'https://portalautos.com.br/' + vehicle.gallery.images[0].path} alt={vehicle.gallery.images[0].name} className="object-cover w-full h-full" />
+                <img src={'https://api.portalautos.com.br/' + vehicle.gallery.images[0].path} alt={vehicle.gallery.images[0].name} className="object-cover w-full h-full" />
             </div>
             <div className="flex flex-col mt-4">
                 <span className="uppercase text-lg font-semibold">{vehicle.version.model.brand.name} {vehicle.version.model.name}</span>
