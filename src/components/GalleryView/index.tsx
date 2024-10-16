056e3a36d730ccfd112bfae1db5ab5e185f40c29
EzequielPires <ezequiel.pires082000@gmail.com>
2024-10-16 18:38:49 -0300

    Commit

--- src/components/GalleryView/index.tsx
+++ src/components/GalleryView/index.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { Keyboard, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
-import { Slide } from "./components/Slide";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { FiX } from "react-icons/fi";
 import { ButtonIcon } from "../buttons/button-icon";
 
@@ -25,13 +26,13 @@ export function GalleryView({ close, active, show, images }) {
 
     return (
         <div className={`fixed top-0 left-0 w-full h-screen z-[9999] bg-black bg-opacity-70`} style={{ visibility: !show ? 'hidden' : 'visible' }}>
-            <div className={'w-full max-w-7xl mx-auto py-8'}>
+            <div className={'w-full max-w-7xl mx-auto py-8 overflow-hidden h-full'}>
                 <div className={`absolute top-4 right-4 z-[9999]`} onClick={close}>
                     <ButtonIcon Icon={FiX} />
                 </div>
                 <Swiper
                     id={'swiper'}
-                    className="w-full"
+                    className="w-full h-full"
                     pagination={{
                         type: "fraction",
                     }}
@@ -44,12 +45,13 @@ export function GalleryView({ close, active, show, images }) {
                         { swiper.isEnd && setState('end') }
                         { !swiper.isBeginning && !swiper.isEnd && setState('progress') }
                     }}
-                    navigation
-                    //initialSlide={active}
+                    navigation={true}
                 >
                     {images.map((item, index) => (
-                        <SwiperSlide key={index} id={'swiper-slide'}>
-                            <Slide image={item} alt={item} />
+                        <SwiperSlide key={index} id={'swiper-slide'} style={{ width: '100% !important' }}>
+                            <div className="w-full h-full flex justify-center items-center">
+                                <img src={'https://api.portalautos.com.br/' + item} alt={item} className="object-contain h-full" />\
+                            </div>
                         </SwiperSlide>
                     ))}
                 </Swiper>
