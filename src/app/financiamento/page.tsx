9a3ca58d5c25e59bff88409097b5b70c0dda57a9
Portal Catalão Internet Service Ltda. <github@portalcatalao.com.br>
2025-08-18 10:38:01 -0300

    Update page.tsx

--- src/app/financiamento/page.tsx
+++ src/app/financiamento/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function FinancingPage() {
     const handleSubmit = async () => {
-        const wpp = "64992496844";
+        const wpp = "5564992496844";
             const text = encodeURIComponent(`Olá, gostaria de simular um financiamento de um veículo.`);
             const whatsappLink = `https://api.whatsapp.com/send?phone=${wpp}&text=${text}`;
 
